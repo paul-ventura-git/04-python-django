99384ba364fd0511c69dba32fb872f2c66ec5d8a
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-15 07:53:56 -0500

    myapp Routes

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def hello(request):
     return HttpResponse("<h1>Hello World</h1>")
+
+def about(request):
+    return HttpResponse("<h1>About</h1>")
